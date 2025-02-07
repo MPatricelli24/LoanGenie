3c9df76a18300f8751ad19ced2add3fece1efb69
MPatricelli24 <mpatricelli24@gmail.com>
2025-02-06 21:18:53 -0500

    Create LandingPage.js

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Welcome to LoanGenie</h1>
+      <p>Instant AI-driven loans in seconds!</p>
+    </div>
+  );
+};
+
+export default LandingPage;
