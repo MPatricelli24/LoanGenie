7b6ad8fee027cd64e2fedfda5b10c92b2f91d951
MPatricelli24 <mpatricelli24@gmail.com>
2025-02-06 21:25:25 -0500

    Create index.js

---
+++ src/pages/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import LoanGenieApp from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LoanGenieApp />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
